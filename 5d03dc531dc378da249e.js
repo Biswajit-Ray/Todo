d026a2ce9099470b295560939c31fd8ff795e648
Biswajit Ray <220484600+Biswajit-Ray@users.noreply.github.com>
2026-06-14 14:07:52 +0000

    Updates

--- 5d03dc531dc378da249e.js
+++
@@ -1,1 +0,0 @@
-import{AppState}from"./state.js";import{renderProjectList,renderTodoList}from"./render.js";import{initializeEventListeners}from"./EventListener.js";import{renderMotivation}from"./motivationLogic.js";import{seedDemoData}from"./state.js";import"./style.css";seedDemoData(),AppState.loadData(),renderMotivation(),renderProjectList(),renderTodoList(),initializeEventListeners();
